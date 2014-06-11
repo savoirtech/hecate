2efe3e6fbfab5319380cee390b73b37a7364c002
Johan Edstrom <jedstrom@savoirtech.com>
2014-06-11 13:47:38 -0400

    Illustrate Composite idea.

---
+++ hecate-cql3/src/test/java/com/savoirtech/hecate/cql3/composite/CompositeColumnFamilyTest.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2014 Savoir Technologies
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.savoirtech.hecate.cql3.composite;
+
+import com.savoirtech.hecate.cql3.dao.PojoDao;
+import com.savoirtech.hecate.cql3.dao.def.DefaultPojoDaoFactory;
+import com.savoirtech.hecate.cql3.test.CassandraTestCase;
+import org.junit.Before;
+import org.junit.Test;
+
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class CompositeColumnFamilyTest extends CassandraTestCase {
+
+    private final static String TABLE =
+        "CREATE TABLE composite (\n" + "id text,\n" + "a text,\n" + "b text,\n" + "c text,\n" + "data text,\n" + "PRIMARY KEY (id, a, b, c)\n" + ");";
+    //----------------------------------------------------------------------------------------------------------------------
+    // Other Methods
+    //----------------------------------------------------------------------------------------------------------------------
+
+    @Before
+    public void createTable() {
+        connect().execute(TABLE);
+    }
+
+    @Test
+    public void testSave() throws Exception {
+        DefaultPojoDaoFactory factory = new DefaultPojoDaoFactory(connect());
+        final PojoDao<String, Composite> dao = factory.createPojoDao(Composite.class);
+        Composite composite = new Composite();
+        composite.setId("ID");
+        composite.setA("us:co:dia");
+        composite.setB("");
+        composite.setC("C");
+        composite.setData("DATA");
+        dao.save(composite);
+
+        Composite found = dao.findByKey(composite.getId());
+        assertNotNull(found);
+        assertEquals("DATA", found.getData());
+        assertEquals(composite.getId(), found.getId());
+
+        System.out.println(connect().execute("select * from composite where  id = 'ID' and a > 'us:co:d';").all());
+    }
+}
